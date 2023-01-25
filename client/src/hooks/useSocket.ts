58549c2b13cd46f6e6e6fbd9e6eada07353ded02
Soonho Kwon <percy3368@gmail.com>
2023-01-26 01:08:01 +0900

    chore(hooks): remove unused hooks using recoil
    remove old version of socket hooks using recoil

--- client/src/hooks/useSocket.ts
+++
@@ -1,9 +0,0 @@
-import { useRecoilValue } from "recoil";
-import { socketState } from "@/store/socket";
-
-/**
- * Provides a readonly socket.io instance.
- * Will return `null` when client is not authenticated
- * since a socket connection requires authentication.
- */
-export const useSocket = () => useRecoilValue(socketState);
