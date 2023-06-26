5a2bd80c2157b2f573fbf0588af1f79669f107ce
Soonho Kwon <percy3368@gmail.com>
2023-06-26 22:58:49 +0900

    feat(type-utils): implement io schema generics

---
+++ interface/utils.ts
@@ -0,0 +1,23 @@
+import { ClientToServerEvents, ServerToClientEvents } from "./events";
+import { Res } from "./helpers";
+
+type AllEvents = ServerToClientEvents & ClientToServerEvents;
+
+export type ClientEventNames = keyof ClientToServerEvents;
+export type ServerEventNames = keyof ServerToClientEvents;
+export type EventNames = keyof AllEvents;
+
+/**
+ * Get the input schema of an event.
+ * @example type InputOf<"chat.send"> = { message: string };
+ */
+export type SchemaOf<Ev extends EventNames> = Parameters<AllEvents[Ev]>[0];
+
+/**
+ * Get the response schema of a client side event
+ * @example type ResponseOf<"chat.send"> = {};
+ */
+export type ResponseOf<Ev extends ClientEventNames>
+  = Parameters<
+  Parameters<ClientToServerEvents[Ev]>[1]
+>[0] extends Res<infer O> ? O : never;
