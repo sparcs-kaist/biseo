0b3584ceb4fab77e65066661080420a41f5f22ce
Soonho Kwon <percy3368@gmail.com>
2023-06-29 18:44:40 +0900

    feat(env): add enviroment var validation

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,45 @@
-import * as process from "process";
-
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import { adminAgendaRouter } from "@/listener/admin.agenda";
 import type { BiseoServer } from "@/types/socket";
+import { authRouter } from "./auth/login";
+import { env } from "./env";
+import { verifyToken } from "./auth/token";
 
 const app = express();
 const httpServer = createServer(app);
 const io: BiseoServer = new Server(httpServer, {
+  path: "/api/socket",
   cors: { origin: "*" },
 });
-const port = process.env.SERVER_PORT ?? 8000;
+const port = env.SERVER_PORT ?? 8000;
 
-app.get("/", (req, res) => {
+app.use(express.json());
+
+app.get("/api", (req, res) => {
   res.send("Hello World!");
 });
 
-adminAgendaRouter.register(io);
+app.use("/api/auth", authRouter);
+
+io.use((socket, next) => {
+  const nickname = verifyToken(socket.handshake.auth.token);
+
+  if (!nickname) {
+    next(new Error("invalid token"));
+    return socket.disconnect();
+  }
+
+  // TODO: check if user is admin
+  socket.emit("init", { nickname, isAdmin: false });
+  next();
+});
+
+io.on("connection", (socket) => {
+
+  adminAgendaRouter.register(io, socket);
+});
 
 httpServer.listen(port);
 console.log(`Server listening on port ${port}`);
