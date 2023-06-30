db303be9324b4373b1fd8bfae9f4b04e2ac09ec7
Soonho Kwon <percy3368@gmail.com>
2023-06-30 15:56:01 +0900

    feat(router): implement protected routes

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import { protect } from "@/utils/routes";
 import { Main, Login, Admin, Layout } from "./pages";
 
 export default createBrowserRouter([
@@ -7,8 +8,17 @@ export default createBrowserRouter([
     element: <Layout />,
     children: [
       { path: "/", element: <Main /> },
-      { path: "/admin", element: <Admin /> },
+      {
+        path: "/admin",
+        element: <Admin />,
+        ...protect({ to: "/", when: user => !user?.isAdmin }),
+      },
     ],
+    ...protect({ to: "/login", when: user => !user }),
   },
-  { path: "/login", element: <Login /> },
+  {
+    path: "/login",
+    element: <Login />,
+    ...protect({ to: "/", when: user => !!user }),
+  },
 ]);
