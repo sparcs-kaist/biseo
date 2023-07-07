9847d6f244f9bff5b2412e7c88dda41e3a4af680
donghyo1234 <donghyo12341234@gmail.com>
2023-07-07 22:48:10 +0900

    feat(admin): add createAgenda

--- client/src/services/admin-agenda.ts
+++ client/src/services/admin-agenda.ts
@@ -1,12 +1,26 @@
 import { socket } from "@/socket";
 import { create } from "zustand";
-import type { AdminAgenda } from "biseo-interface/admin/agenda";
+import type { AdminAgenda, AdminAgendaCreate } from "biseo-interface/admin/agenda";
 
 
 interface AdminAgendaState {
     adminAgendas: AdminAgenda[];
+    createAgenda: (agenda : AdminAgendaCreate) => void;
 }
 
 const useAdminAgenda = create<AdminAgendaState>((set,get) => ({
     adminAgendas: [],
+
+    createAgenda: async (agenda) => {
+        try {
+            await socket.emitAsync("admin.agenda.create",agenda);
+        } catch (error){
+            // TODO: handle error
+        }
+    },
+
+
 }))
+
+
+
