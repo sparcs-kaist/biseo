c2f138ea73ec833ac253817b3e5ec4599a24f5a6
donghyo1234 <donghyo12341234@gmail.com>
2023-07-11 00:07:38 +0900

    style(admin): reformat

--- client/src/services/admin-agenda.ts
+++ client/src/services/admin-agenda.ts
@@ -1,32 +1,36 @@
 import { socket } from "@/socket";
 import { create } from "zustand";
-import type { AdminAgenda, AdminAgendaCreate } from "biseo-interface/admin/agenda";
-
+import type {
+  AdminAgenda,
+  AdminAgendaCreate,
+} from "biseo-interface/admin/agenda";
 
 interface AdminAgendaState {
-    adminAgendas: AdminAgenda[];
-    createAgenda: (agenda : AdminAgendaCreate) => void;
-    retrieveAll: () => void;
+  adminAgendas: AdminAgenda[];
+  createAgenda: (agenda: AdminAgendaCreate) => void;
+  retrieveAll: () => void;
 }
 
-const useAdminAgenda = create<AdminAgendaState>((set,get) => ({
-    adminAgendas: [],
+const useAdminAgenda = create<AdminAgendaState>((set, get) => ({
+  adminAgendas: [],
 
-    createAgenda: async (agenda) => {
-        try {
-            await socket.emitAsync("admin.agenda.create",agenda);
-        } catch (error){
-            // TODO: handle error
-        }
-    },
+  createAgenda: async (agenda) => {
+    try {
+      await socket.emitAsync("admin.agenda.create", agenda);
+    } catch (error) {
+      // TODO: handle error
+    }
+  },
 
-    retrieveAll: async () => {
-        try {
-            const retAdminAgendas = await socket.emitAsync("admin.agenda.retrieveAll",{})
-            set({adminAgendas:retAdminAgendas});
-        } catch(error){
-            // TODO: handle error
-        }
-    },
-
-}))
+  retrieveAll: async () => {
+    try {
+      const retAdminAgendas = await socket.emitAsync(
+        "admin.agenda.retrieveAll",
+        {}
+      );
+      set({ adminAgendas: retAdminAgendas });
+    } catch (error) {
+      // TODO: handle error
+    }
+  },
+}));
