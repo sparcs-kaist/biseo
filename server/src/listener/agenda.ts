2822ca17600c61c638d1a8531c322a462121542b
Ariaspect <keeminjoon@kaist.ac.kr>
2023-07-17 03:12:03 +0900

    feat(agenda): service, listener logic complete

--- server/src/listener/agenda.ts
+++ server/src/listener/agenda.ts
@@ -1,4 +1,5 @@
 import * as schema from "biseo-interface/agenda";
+import { retrieveAll, vote } from "@/service/agenda";
 import { BiseoError } from "@/lib/error";
 
 import { Router } from "@/lib/listener";
@@ -6,9 +7,15 @@ import { Router } from "@/lib/listener";
 const router = Router();
 
 router.on("agenda.retrieveAll", schema.RetrieveAll, async (req, { io }) => {
-  return {};
+  const res = await retrieveAll(req);
+  if (!res) throw new BiseoError("failed to retrieve agenda");
+  return res;
 });
 
 router.on("agenda.vote", schema.Vote, async (req, { io }) => {
+  const res = await vote(req);
+  if (!res) throw new BiseoError("failed to vote");
   return {};
 });
+
+export { router as agendaRouter };
--- server/src/service/agenda.ts
+++ server/src/service/agenda.ts
@@ -2,7 +2,7 @@ import type { Prisma } from "@prisma/client";
 import * as schema from "biseo-interface/agenda";
 import { prisma } from "@/db/prisma";
 
-export const retrieveAll = async (): Promise<schema.RetrieveAllCb | null> => {
+export const retrieveAll = async ({}): Promise<schema.RetrieveAllCb | null> => {
   const agenda_dbres = await prisma.agenda.findMany({
     where: { deletedAt: null },
     include: {
@@ -17,30 +17,44 @@ export const retrieveAll = async (): Promise<schema.RetrieveAllCb | null> => {
     },
   });
   const res = agenda_dbres.map((agenda): schema.Agenda => {
-    let status: "ongoing" | "preparing" | "terminated" = "ongoing";
+    let status: "ongoing" | "terminated" = !agenda.endAt
+      ? "ongoing"
+      : "terminated";
     return {
+      // TODO: union ongoing | terminated agenda does not match with status
       id: agenda.id,
       title: agenda.title,
       content: agenda.content,
       resolution: agenda.subtitle,
       status: status,
+      voters: {
+        voted: agenda.choices.reduce(
+          (acc, choice) => acc + choice.users.length,
+          0
+        ),
+        total: agenda.voters.length,
+      },
+      voted: null, // TODO: How to know `voted` without `user`?
       choices: agenda.choices.map((choice) => {
         return {
           id: choice.id,
           name: choice.name,
         };
       }),
-      voters: {
-        voted: 0, // TODO
-        total: agenda.voters.length,
-      },
     };
   });
   return null;
 };
 
 export const vote = async ({
-  choiceId,
+  choiceId, // TODO: Is `choiceId` unique among choices?
 }: schema.Vote): Promise<schema.VoteCb> => {
+  // TODO: check Votable (necessary?)
+  const res = prisma.userChoice.create({
+    data: {
+      userId: 0, // TODO: How to get userId without `user`?
+      choiceId: choiceId,
+    },
+  });
   return {};
 };
