63bc2a78be84e7f6bbf1c3f4794b3aef60fd4c29
ariaspect <keeminjoon@kaist.ac.kr>
2023-07-17 00:27:24 +0900

    feat(agenda): retrieveAll service parts

--- server/src/service/agenda.ts
+++ server/src/service/agenda.ts
@@ -2,7 +2,40 @@ import type { Prisma } from "@prisma/client";
 import * as schema from "biseo-interface/agenda";
 import { prisma } from "@/db/prisma";
 
-export const retrieveAll = async ({}): Promise<schema.RetrieveAllCb | null> => {
+export const retrieveAll = async (): Promise<schema.RetrieveAllCb | null> => {
+  const agenda_dbres = await prisma.agenda.findMany({
+    where: { deletedAt: null },
+    include: {
+      voters: true,
+      choices: {
+        select: {
+          id: true,
+          name: true,
+          users: true,
+        },
+      },
+    },
+  });
+  const res = agenda_dbres.map((agenda): schema.Agenda => {
+    let status: "ongoing" | "preparing" | "terminated" = "ongoing";
+    return {
+      id: agenda.id,
+      title: agenda.title,
+      content: agenda.content,
+      resolution: agenda.subtitle,
+      status: status,
+      choices: agenda.choices.map((choice) => {
+        return {
+          id: choice.id,
+          name: choice.name,
+        };
+      }),
+      voters: {
+        voted: 0, // TODO
+        total: agenda.voters.length,
+      },
+    };
+  });
   return null;
 };
 
