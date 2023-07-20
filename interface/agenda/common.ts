0052a807204a0827af8b6e77a857d00b22d4326d
Ariaspect <keeminjoon@kaist.ac.kr>
2023-07-20 21:45:35 +0900

    feat(interface): add votable field to agenda

--- interface/agenda/common.ts
+++ interface/agenda/common.ts
@@ -49,7 +49,10 @@ export type AgendaBase = z.infer<typeof AgendaBase>;
  */
 export const OngoingAgenda = AgendaBase.extend({
   status: z.enum(["ongoing"]),
-  voted: z.number().nullable(), // choiceId | null
+  user: z.object({
+    votable: z.boolean(),
+    voted: z.number().nullable(), // choiceId | null
+  }),
   choices: z.array(Choice),
 });
 export type OngoingAgenda = z.infer<typeof OngoingAgenda>;
