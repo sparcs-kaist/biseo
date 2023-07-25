b743ebf9ab103a54af8d7d4afa7ad40be1ece446
withsang <jsa5115@naver.com>
2023-07-26 06:27:05 +0900

    feat(winston): console transport on production

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -36,6 +36,15 @@ const datePattern = "YYYY-MM-DD-HH";
 // 로그 파일당 최대 크기(=5MB).
 const maxSize = 5 * 1024 * 1024;
 
+/**
+ * console.log()와 console.error() 대신 사용되는 winston Logger 객체입니다.
+ *
+ * - "production" 환경: 모든 로그는 파일 시스템에 저장되고, 오류 메시지는 콘솔로도 출력됩니다.
+ * - "development" & "test" 환경: 모든 로그는 콘솔에 출력됩니다.
+ *
+ * @method info(message: string, callback: winston.LogCallback) - 일반적인 정보(API 접근 등) 기록을 위해 사용합니다.
+ * @method error(message: string, callback: winston.LogCallback)  - 오류 메시지를 기록하기 위해 사용합니다.
+ */
 const logger =
   env.NODE_ENV === "production"
     ? // "production" 환경에서 사용되는 Logger 객체
@@ -44,28 +53,32 @@ const logger =
         format: uncolorizedFormat,
         defaultMeta: { service: "biseo" },
         transports: [
-          // 전체 로그("info", "warn", "error")를 저장합니다.
+          // 전체 로그("info", "warn", "error")를 파일로 출력합니다.
           new dailyRotateFileTransport({
             level: "info",
             filename: path.resolve("logs/%DATE%-combined.log"),
             datePattern,
             maxSize,
           }),
-          // 예외 처리로 핸들링 된 오류 로그("error")를 저장합니다.
+          // 예외 처리로 핸들링 된 오류 로그("error")를 파일과 콘솔에 출력합니다.
           new dailyRotateFileTransport({
             level: "error",
             filename: path.resolve("logs/%DATE%-error.log"),
             datePattern,
             maxSize,
           }),
+          new transports.Console({
+            level: "error",
+          }),
         ],
         exceptionHandlers: [
-          // 예외 처리가 되지 않은 오류 로그("error")를 저장합니다.
+          // 예외 처리가 되지 않은 오류 로그("error")를 파일과 콘솔에 출력합니다.
           new dailyRotateFileTransport({
             filename: path.resolve("logs/%DATE%-unhandled.log"),
             datePattern,
             maxSize,
           }),
+          new transports.Console(),
         ],
       })
     : // "development", "test" 환경에서 사용되는 Logger 객체
@@ -77,11 +90,4 @@ const logger =
         exceptionHandlers: [new transports.Console()],
       });
 
-/**
- * console.log()와 console.error() 대신 사용되는 winston Logger 객체입니다.
- * production 환경에서는 파일 시스템에 로그가 저장되고, development와 test 환경에서는 콘솔에 로그가 출력됩니다.
- *
- * @method info(message: string, callback: winston.LogCallback) - 일반적인 정보(API 접근 등) 기록을 위해 사용합니다.
- * @method error(message: string, callback: winston.LogCallback)  - 오류 메시지를 기록하기 위해 사용합니다.
- */
 export { logger };
