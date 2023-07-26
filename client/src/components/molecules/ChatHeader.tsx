02afea54432bf3775e50e92a82a6419b56a36456
Nicole Lee <nicolelee2001@gmail.com>
2023-07-26 23:27:41 +0900

    feat(ui): refactor ChatHeader code

--- client/src/components/molecules/ChatHeader.tsx
+++ client/src/components/molecules/ChatHeader.tsx
@@ -1,22 +1,25 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { Box, Text } from "../atoms";
+import { Text } from "../atoms";
 
 interface Props {
   title: string;
 }
 
 const Container = styled.div`
+  display: flex;
   width: 100%;
+  height: 42px;
+  background-color: ${props => props.theme.colors.gray100};
+  align-items: center;
+  padding: 0 20px;
   border-bottom: solid 1px ${props => props.theme.colors.gray200};
 `;
 
 export const ChatHeader: React.FC<Props> = ({ title }) => {
   return (
     <Container>
-      <Box w="fill" h={42} bg="gray100" justify="center" padHorizontal={20}>
-        <Text variant="title2">{title}</Text>
-      </Box>
+      <Text variant="title2">{title}</Text>
     </Container>
   );
 };
