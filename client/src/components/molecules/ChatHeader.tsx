a358ba0d8552f7722441747cdcff618d83077f2b
Nicole Lee <nicolelee2001@gmail.com>
2023-07-26 23:46:55 +0900

    chore(ui): fix input route

--- client/src/components/molecules/ChatHeader.tsx
+++ client/src/components/molecules/ChatHeader.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { Text } from "../atoms";
+import { Text } from "@/components/atoms";
 
 interface Props {
   title: string;
