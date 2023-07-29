a1eb7fdf5daf13b02f98d302b3f6f00e318af267
Ariaspect <keeminjoon@kaist.ac.kr>
2023-07-29 23:46:28 +0900

    fix(ui): adjust choice props

--- client/src/components/molecules/ChoicePercentage.tsx
+++ client/src/components/molecules/ChoicePercentage.tsx
@@ -2,31 +2,30 @@ import React, { type PropsWithChildren } from "react";
 import { Box, Text } from "@/components/atoms";
 import type { Color } from "@/theme";
 
-interface Props {
-  choiceName: string;
-  choiceCount: number;
-  choiceTotal: number;
+interface Choice {
+  name: string;
+  count: number;
   color: Color;
 }
 
-export const SectionHeader: React.FC<Props> = ({
-  choiceName,
-  choiceCount,
-  choiceTotal,
-  color,
-}) => {
+interface Props {
+  choice: Choice;
+  total: number;
+}
+
+export const ChoicePercentage: React.FC<Props> = ({ choice, total }) => {
   return (
     //bg color of squares needed, according to the rank or taking color as parameter directly
-    <Box w={260} h={14} align="center" dir="row" padHorizontal={20}>
-      <Box justify="space-betweeen">
-        <Box w={12} h={12} round={2} bg={0} color={color}></Box>
-        <Text variant="option1">{choiceName}</Text>
+    <Box w={260} h={14} justify="space-between" dir="row">
+      <Box w={39} align="center" dir="row" gap={8}>
+        <Box w={12} h={12} round={2} bg={choice.color}></Box>
+        <Text variant="option1">{choice.name}</Text>
       </Box>
       <Text variant="option1" color="gray500">
-        {choiceCount}/{choiceTotal}
+        {choice.count}/{total}
       </Text>
       <Text variant="option1" color="gray500">
-        {(choiceCount / choiceTotal) * 100}%
+        {(choice.count / total) * 100}%
       </Text>
     </Box>
   );
