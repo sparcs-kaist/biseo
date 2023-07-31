f548a78e5f8ce0ab273cbf4fd8803d06cf24247b
minjoo0729 <minjoo0729@kaist.ac.kr>
2023-07-31 10:35:47 +0900

    fix(ui): add colors based on Figma

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -6,20 +6,24 @@ export const theme = {
     gray100: "#FAFAFA",
     gray200: "#EEEEEE",
     gray300: "#D9D9D9",
-    gray400: "#8C8C8C",
-    gray500: "#555555",
+    gray400: "#B6B6B6",
+    gray500: "#8C8C8C",
+    gray600: "#555555",
     blue100: "#FAFCFF",
     blue200: "#E7F0FF",
     blue300: "#BFDCFF",
     blue400: "#6EABF4",
-    blue500: "#0066AE",
-    blue600: "#004B81",
-    // added by ella
-    // purple200: purple-light
-    // purple600: purple
-    gray600: "555555",
-    purple200: "#F6EEFE",
-    purple600: "#9836EF",
+    blue500: "#2F80DE",
+    blue600: "#065DAC",
+    blue700: "#004B81",
+    // added by ella_start
+    purple200: "#F6EEFE",   // purple-light
+    purple600: "#9836EF",   // purple
+    orange200: "FDF7E6",    // orange-light
+    orange600: "FF9211",    // orange
+    green200: "E9F9EF",     // green-light
+    green600: "008B1C",     // green
+    // added by ella_end
   } as const,
 } as const;
 
