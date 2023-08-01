cf50af0c8cd6773cce0e9fe2cdc26b32835a2ed5
Ariaspect <keeminjoon@kaist.ac.kr>
2023-08-02 01:22:31 +0900

    feat(tag): adjust tag to use type field

--- client/src/components/atoms/Tag.tsx
+++ client/src/components/atoms/Tag.tsx
@@ -1,12 +1,41 @@
+import React from "react";
 import styled from "@emotion/styled";
 import { css } from "@emotion/react";
+import { theme } from "@/theme";
 import type { Color } from "@/theme";
 
-export const Tag = styled.div<{
-  public?: boolean;
-  anonymous?: boolean;
-  votable?: boolean;
-  bg?: Color;
+const colors = (color: Color, bg: Color) =>
+  css({
+    color: theme.colors[color],
+    backgroundColor: theme.colors[bg],
+  });
+
+const tagNames = {
+  _hidden: "공개",
+  hidden: "비공개",
+  _anonymous: "기명",
+  anonymous: "무기명",
+  // _votable: colors("green600", "green200"),
+  votable: "투표 가능",
+  empty: "",
+} as const;
+
+const tagStyles = {
+  _hidden: colors("orange600", "orange200"),
+  hidden: colors("gray600", "gray200"),
+  _anonymous: colors("green600", "green200"),
+  anonymous: colors("purple600", "purple200"),
+  // _votable: colors("green600", "green200"),
+  votable: colors("blue600", "blue200"),
+  empty: css`
+    display: none;
+  `,
+} as const;
+
+type Types = keyof typeof tagStyles;
+
+const TagInner = styled.div<{
+  type: Types;
   round?: number;
   align?: "center" | "start" | "end";
   justify?: "center" | "start" | "end";
@@ -18,10 +47,7 @@ export const Tag = styled.div<{
   padRight?: number;
 }>(
   ({
-    public = false,
-    anonymous,
-    votable,
-    bg,
+    type,
     round = 3,
     align = "center",
     justify = "center",
@@ -32,15 +58,29 @@ export const Tag = styled.div<{
     padBottom = padVertical,
     padTop = padVertical,
     theme,
-  }) => css`
-    background-color: ${bg ? theme.colors[bg] : "transparent"};
-    border-radius: ${round}px;
-    display: flex;
-    align-items: ${align};
-    justify-content: ${justify};
-    padding-top: ${padTop}px;
-    padding-bottom: ${padBottom}px;
-    padding-left: ${padLeft}px;
-    padding-right: ${padRight}px;
-  `,
+  }) => [
+    css`
+      height: 18px;
+      border-radius: ${round}px;
+      display: flex;
+      align-items: ${align};
+      justify-content: ${justify};
+      padding-top: ${padTop}px;
+      padding-bottom: ${padBottom}px;
+      padding-left: ${padLeft}px;
+      padding-right: ${padRight}px;
+
+      font-size: 10px;
+      font-weight: 500;
+    `,
+    tagStyles[type],
+  ],
 );
+
+interface Props {
+  type: Types;
+}
+
+export const Tag: React.FC<Props> = ({ type }) => {
+  return <TagInner type={type}>{tagNames[type]}</TagInner>;
+};
