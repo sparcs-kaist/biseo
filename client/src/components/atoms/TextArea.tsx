ee88199f35fa45cd9e1f0823a290d73a3b1481fa
Nicole Lee <nicolelee2001@gmail.com>
2023-08-02 02:31:33 +0900

    refactor(ui): textarea implementation

--- client/src/components/atoms/TextArea.tsx
+++ client/src/components/atoms/TextArea.tsx
@@ -1,18 +1,10 @@
-import React, { useState } from "react";
 import styled from "@emotion/styled";
 import { Text } from "@/components/atoms";
 
-export const Container = styled(Text)`
+// TODO: max char limit, emoticon and hyperlink
+export const TextArea = styled(Text)`
   width: 100%;
   outline: none;
 `;
 
-export const TextArea: React.FC = () => {
-  const [message, setMessage] = useState<string>("");
-  return (
-    <Container variant="body" onInput={() => {}} contentEditable>
-      {/* TODO: max char limit, emoticon and hyperlink */}
-      {message}
-    </Container>
-  );
-};
+TextArea.defaultProps = { contentEditable: true };
