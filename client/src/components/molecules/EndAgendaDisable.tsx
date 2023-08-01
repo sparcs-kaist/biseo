ea26455ee507c31c56d226cbe5db785f2ee9fa9d
Ariaspect <keeminjoon@kaist.ac.kr>
2023-08-02 01:19:02 +0900

    refactor(ui): merge folded into agenda card

--- client/src/components/molecules/EndAgendaDisable.tsx
+++
@@ -1,4 +0,0 @@
-import { Box, Text } from "@/components/atoms";
-export const EndAgendaDisable: React.FC = () => {
-  return <Box></Box>;
-};
--- client/src/components/pages/MainPage.tsx
+++ client/src/components/pages/MainPage.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Box } from "@/components/atoms";
-import { AgendaFoldedCard, ChatSection } from "@/components/organisms";
+import { AgendaSection, ChatSection } from "@/components/organisms";
 
 export const MainPage: React.FC = () => (
   <Box dir="row" padTop={10} padBottom={30} gap={20}>
-    <AgendaFoldedCard />
+    <AgendaSection />
     <ChatSection />
   </Box>
 );
