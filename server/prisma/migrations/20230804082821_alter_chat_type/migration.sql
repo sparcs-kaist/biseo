a3b5f7823bb0f94ce8b79c506e1b27199cad7de1
Soonho Kwon <percy3368@gmail.com>
2023-08-06 11:18:43 +0900

    feat(migrate): add chat type migration

---
+++ server/prisma/migrations/20230804082821_alter_chat_type/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - The values [Message,Alarm] on the enum `chat_type` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE ` chat ` MODIFY ` type ` ENUM ('message', 'notice') NOT NULL;
