193b55114ef93517ea20b8f7a3106dd4fc34b1bd
Nicole Lee <nicolelee2001@gmail.com>
2023-08-07 22:35:49 +0900

    feat(ui): add layout to admin pages

--- client/src/components/pages/AdminAgendaPage.tsx
+++ client/src/components/pages/AdminAgendaPage.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { Box } from "@/components/atoms";
 
-export const AdminAgendaPage: React.FC = () => {
-  return <div>admin agenda</div>;
-};
+export const AdminAgendaPage: React.FC = () => (
+  <Box dir="row" padTop={20} padBottom={30} gap={20}>
+    <Box w={300} h="fill">
+      Section 1
+    </Box>
+    <Box w={300} h="fill">
+      Section 2
+    </Box>
+    <Box w={300} h="fill">
+      Section 3
+    </Box>
+  </Box>
+);
--- client/src/components/pages/AdminSettingPage.tsx
+++ client/src/components/pages/AdminSettingPage.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Box } from "@/components/atoms";
 
-export const AdminSettingPage: React.FC = () => {
-  return <div>admin setting</div>;
-};
+export const AdminSettingPage: React.FC = () => (
+  <Box dir="row" padTop={20} padBottom={30} gap={20}>
+    <Box w={380} h="fill">
+      Section 1
+    </Box>
+    <Box w={380} h="fill">
+      Section 2
+    </Box>
+  </Box>
+);
