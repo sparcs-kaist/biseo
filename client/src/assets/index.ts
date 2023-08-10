eecd62b8243e6e1a6e9126958cee03124170ae8b
Nicole Lee <nicolelee2001@gmail.com>
2023-08-11 01:45:31 +0900

    fix(bug): missing export

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,4 +1,5 @@
 export { ReactComponent as CloseIcon } from "./close.svg";
 export { ReactComponent as EmoticonIcon } from "./emoticon.svg";
 export { ReactComponent as LogoIcon } from "./logo.svg";
+export { ReactComponent as SelectIcon } from "./select.svg";
 export { ReactComponent as SendIcon } from "./send.svg";
