864ba77eb5eab9f6ff311becc83635cb8f214ba4
Ariaspect <keeminjoon@kaist.ac.kr>
2023-08-13 03:26:51 +0900

    feat: add progress bar atom

---
+++ client/src/components/atoms/ProgressBar.tsx
@@ -0,0 +1,34 @@
+import styled from "@emotion/styled";
+
+const ProgressContainer = styled.div<{ width?: number }>(
+  ({ width = 180, theme }) => `
+  display: flex;
+  align-items: center;
+
+  progress {
+    appearance: none;
+    width: ${width}px;
+    height:12px;
+
+    ::-webkit-progress-bar {
+      border-radius: 5px;
+      background-color: ${theme.colors["blue200"]};;
+    }
+    ::-webkit-progress-value {
+      border-radius: 5px 0px 0px 5px;
+      background-color: ${theme.colors["blue400"]};
+    }
+  }
+`,
+);
+
+interface ProgressProps {
+  value: number;
+  max: number;
+}
+
+export const ProgressBar = ({ value, max }: ProgressProps) => (
+  <ProgressContainer>
+    <progress value={value} max={max} />
+  </ProgressContainer>
+);
