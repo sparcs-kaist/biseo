f64a97acc3b191b5f8c50dd39ea257a853d616ac
Ariaspect <keeminjoon@kaist.ac.kr>
2023-08-13 03:26:14 +0900

    refactor: rename to resolution field

--- client/src/components/molecules/AgendaDetail.tsx
+++ client/src/components/molecules/AgendaDetail.tsx
@@ -3,23 +3,27 @@ import { Box, Text } from "@/components/atoms";
 
 interface Props {
   title: string;
-  subtitle: string;
   content: string;
+  resolution: string;
 }
 
-export const AgendaDetail: React.FC<Props> = ({ title, subtitle, content }) => (
+export const AgendaDetail: React.FC<Props> = ({
+  title,
+  content,
+  resolution,
+}) => (
   <>
     <Box w={260} gap={2}>
       <Text variant="title2" color="black">
         {title}
       </Text>
       <Text variant="subtitle" color="gray500">
-        {subtitle}
+        {content}
       </Text>
     </Box>
     <Box>
       <Text variant="subtitle" color="blue600">
-        {content}
+        {resolution}
       </Text>
     </Box>
   </>
