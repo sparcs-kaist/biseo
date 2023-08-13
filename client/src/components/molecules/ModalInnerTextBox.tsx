6f4b4f93c64f14ff6c4d911c1a3a977dd60b7d86
prectal123 <prectal123@naver.com>
2023-08-14 00:33:45 +0900

    feat(ui): add modal inner text box

---
+++ client/src/components/molecules/ModalInnerTextBox.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BorderedBox, Box, Text, TextArea } from "../atoms";
+
+interface Props {
+  title: string;
+  content: string;
+}
+
+export const ModalInnerTextBox: React.FC<Props> = ({ title, content }) => (
+  <Box dir="column" gap={8}>
+    <Text variant="body" color="black">
+      {title}
+    </Text>
+    <BorderedBox
+      borderColor="gray200"
+      bg="white"
+      w={300}
+      borderSize={3}
+      padVertical={10}
+      padHorizontal={15}
+      round={5}
+      borderStyle="solid"
+    >
+      <Text color="gray600" variant="subtitle">
+        {content}
+      </Text>
+    </BorderedBox>
+  </Box>
+);
