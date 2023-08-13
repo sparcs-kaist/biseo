046e5e2f633297761c351bc7f4ff371012f79cae
Ariaspect <keeminjoon@kaist.ac.kr>
2023-08-14 03:04:16 +0900

    feat: add admin ongoing agenda

--- client/src/components/organisms/OngoingAgendaModal.tsx
+++ client/src/components/organisms/OngoingAgendaModal.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Modal } from "@/components/molecules";
+import { Box } from "@/components/atoms";
 
 export const OngoingAgendaModal: React.FC = () => (
-  <Modal title="진행 중인 투표"></Modal>
+  <Modal title="진행 중인 투표">
+    <Box></Box>
+  </Modal>
 );
