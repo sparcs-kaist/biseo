397a1bd2a02d831b7ba4e3df5920f88ac10d99cf
prectal123 <prectal123@naver.com>
2023-08-14 00:34:28 +0900

    feat(ui): add modal participate percentage bar

---
+++ client/src/components/molecules/ParticipantBar.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { BorderedBox, Box, Text, TextArea } from "../atoms";
+
+interface Props {
+  total: number;
+  participant: number;
+}
+
+export const ParticipantBar: React.FC<Props> = ({ total, participant }) => (
+  <Box w={300} h={59} gap={8} dir="column">
+    <Box w={75} h={20} gap={8} dir="row">
+      <Text variant="body" color="black">
+        투표 현황
+      </Text>
+      <Box bg="blue200" round={5} align="center" justify="center" w={20} h={20}>
+        <Text color="blue600">{participant}</Text>
+      </Box>
+    </Box>
+    <BorderedBox
+      w={300}
+      h={30}
+      bg="white"
+      round={5}
+      dir="row"
+      align="center"
+      justify="space-between"
+      borderSize={1}
+      borderStyle="solid"
+      borderColor="gray200"
+      color="gray200"
+      position="relative"
+    >
+      <Box
+        w={(300 * participant) / total}
+        bg="blue300"
+        h={30}
+        z-index={0}
+        position="absolute"
+      ></Box>
+      <Box
+        z-index={2}
+        position="absolute"
+        w={300}
+        h={30}
+        dir="row"
+        align="center"
+        justify="space-between"
+        padHorizontal={13}
+        padVertical={6}
+      >
+        <Text color="gray500" variant="option1">
+          {(participant * 100) / total}%
+        </Text>
+        <Text color="gray500" variant="option1">
+          {participant}/{total}
+        </Text>
+      </Box>
+    </BorderedBox>
+  </Box>
+);
