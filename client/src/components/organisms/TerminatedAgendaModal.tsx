15edaba5463aeeaebf7b06dcf98f9f8ca5197e19
prectal123 <prectal123@naver.com>
2023-08-15 00:38:58 +0900

    fix(ui): replace modal inner subcomponents

--- client/src/components/organisms/TerminatedAgendaModal.tsx
+++ client/src/components/organisms/TerminatedAgendaModal.tsx
@@ -4,6 +4,7 @@ import {
   ModalInnerTextBox,
   OptionVoteResult,
   ParticipantBar,
+  TerminatedModalInner,
 } from "@/components/molecules";
 import { BorderedBox, Box, Table, Text, TextArea } from "../atoms";
 
@@ -12,35 +13,40 @@ export const TerminatedAgendaModal: React.FC = () => (
     <Box h={498} w={630} justify="space-between" dir="row">
       <Box w={300} h={498} gap={20} dir="column">
         <Box w={300} gap={10}>
-          <ModalInnerTextBox
-            title="투표 제목"
-            content="이혜원 회원의 승급 심사"
-          />
-          <ModalInnerTextBox
-            title="투표 내용"
-            content="이혜원 회원의 승급 심사입니다 참 재밌겠죠? 사실 안봐도 승급 확정이긴합니다 이걸 왜 하냐고요?"
-          />
-          <ModalInnerTextBox title="의결 문안" content="글쎄요" />
+          <TerminatedModalInner.TextBoxWithTitle title="투표 제목">
+            이혜원 회원의 승급 심사
+          </TerminatedModalInner.TextBoxWithTitle>
+          <TerminatedModalInner.TextBoxWithTitle title="투표 내용">
+            이혜원 회원의 승급 심사입니다 참 재밌겠죠? 사실 안봐도 승급
+            확정이긴합니다 이걸 왜 하냐고요
+          </TerminatedModalInner.TextBoxWithTitle>
+          <TerminatedModalInner.TextBoxWithTitle title="의결 문안">
+            글쎄요
+          </TerminatedModalInner.TextBoxWithTitle>
         </Box>
         <Box w={300} h={245} gap={10}>
-          <ParticipantBar total={10} participant={7} />
+          <TerminatedModalInner.ParticipantBar total={10} participant={6} />
           <Box w={300} h={177} dir="column" gap={8}>
-            <Text variant="body" color="black">
-              투표 결과
-            </Text>
-            <BorderedBox
-              w={300}
-              borderColor="gray200"
-              borderSize={1}
-              borderStyle="solid"
-              round={5}
-              pad={10}
-              gap={10}
-              dir="column"
-            >
-              <OptionVoteResult name="찬성" count={5} totalCount={7} w={280} />
-              <OptionVoteResult name="반대" count={2} totalCount={7} w={280} />
-            </BorderedBox>
+            <TerminatedModalInner.OptionResultsBox>
+              <TerminatedModalInner.OptionVoteResult
+                name="찬성"
+                count={5}
+                totalCount={7}
+                w={280}
+              />
+              <TerminatedModalInner.OptionVoteResult
+                name="반대"
+                count={1}
+                totalCount={7}
+                w={280}
+              />
+              <TerminatedModalInner.OptionVoteResult
+                name="몰루"
+                count={1}
+                totalCount={7}
+                w={280}
+              />
+            </TerminatedModalInner.OptionResultsBox>
           </Box>
         </Box>
       </Box>
