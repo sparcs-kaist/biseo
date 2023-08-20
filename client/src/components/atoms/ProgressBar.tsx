559cc64e928963da0bed6e4862e6a71b737d3cde
minjoo0729 <minjoo0729@kaist.ac.kr>
2023-08-20 23:03:36 +0900

    fix(progressBar): errors about ui and NaN

--- client/src/components/atoms/ProgressBar.tsx
+++ client/src/components/atoms/ProgressBar.tsx
@@ -5,23 +5,20 @@ const ProgressContainer = styled.div<{ width?: number }>(
   ({ width = 180, theme }) => `
   display: flex;
   align-items: center;
-
-  progress {
-    appearance: none;
-    width: ${width}px;
-    height:12px;
-
-    ::-webkit-progress-bar {
-      border-radius: 5px;
-      background-color: ${theme.colors["blue200"]};;
-    }
-    ::-webkit-progress-value {
-      border-radius: 5px 0px 0px 5px;
-      background-color: ${theme.colors["blue400"]};
-    }
-  }
-`,
+  width: ${width}px;
+  height: 12px;
+  background-color: ${theme.colors["blue200"]};
+  border-radius: 5px;
+  overflow: hidden;
+`
 );
+
+const ProgressBarFill = styled.div<{ value: number; max: number }>`
+  height: 100%;
+  background-color: ${props => props.theme.colors["blue400"]};
+  width: ${props => props.max ? (props.value / props.max) * 100 : 0}%;
+  border-radius: 5px ${props => props.value === props.max ? '5px' : '0px'} 0px 5px;
+`;
 
 interface ProgressProps {
   value: number;
@@ -30,6 +27,6 @@ interface ProgressProps {
 
 export const ProgressBar: React.FC<ProgressProps> = ({ value, max }) => (
   <ProgressContainer>
-    <progress value={value} max={max} />
+    <ProgressBarFill value={value} max={max} />
   </ProgressContainer>
 );
