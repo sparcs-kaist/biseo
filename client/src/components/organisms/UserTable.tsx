dfee569fccc450f8a4bc2a0f720c864d6b05074a
Nicole Lee <nicolelee2001@gmail.com>
2023-08-22 02:06:09 +0900

    feat(table): add editable option and get selected

--- client/src/components/organisms/UserTable.tsx
+++ client/src/components/organisms/UserTable.tsx
@@ -1,18 +1,33 @@
 import React, { useEffect, useState } from "react";
-import { Header, Table, Cell, Row, CheckBox, Text } from "@/components/atoms";
+import type { AdminUser } from "biseo-interface/admin/user";
+import { Header, Table, Cell, Row, CheckBox } from "@/components/atoms";
 import { useAdminUser } from "@/services/admin-user";
 
-export const UserTable: React.FC = () => {
+interface Props {
+  userList?: AdminUser[];
+  selected?: number[];
+  editable?: boolean;
+}
+
+export const UserTable: React.FC<Props> = ({
+  userList,
+  selected = [],
+  editable = false,
+}) => {
   const { users, retrieveUsers } = useAdminUser(state => ({
     users: state.adminUsers,
     retrieveUsers: state.retrieveAll,
   }));
 
+  const [displayUsers, setDisplayUsers] = useState(userList);
   useEffect(() => {
     retrieveUsers();
+    if (!userList) {
+      setDisplayUsers(users);
+    }
   }, []);
 
-  const [selectedUsers, setSelectedUsers] = useState<number[]>([]);
+  const [selectedUsers, setSelectedUsers] = useState(selected);
   const selectUser = (id: number) => {
     if (selectedUsers.includes(id)) {
       setSelectedUsers(selectedUsers.filter(user => user !== id));
@@ -31,10 +46,10 @@ export const UserTable: React.FC = () => {
         <Cell w={120}>닉네임</Cell>
         <Cell>태그</Cell>
       </Header>
-      {users?.map(user => (
+      {displayUsers?.map(user => (
         <Row
           selected={selectedUsers.includes(user.id)}
-          onClick={() => selectUser(user.id)}
+          onClick={() => editable && selectUser(user.id)}
         >
           <Cell w={27} onClick={() => ({})}>
             <CheckBox checked={selectedUsers.includes(user.id)} />
