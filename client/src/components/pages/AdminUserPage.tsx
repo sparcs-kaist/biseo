7549c0663388cda163038a860d102ab3a1ec6fcd
minjoo0729 <minjoo0729@kaist.ac.kr>
2023-08-22 16:11:57 +0900

    fix(userpage): change to setting page

--- client/src/components/pages/AdminUserPage.tsx
+++ client/src/components/pages/AdminUserPage.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { Box } from "@/components/atoms";
-import { AdminUserSection } from "../organisms/AdminUserSection";
 
 export const AdminUserPage: React.FC = () => (
   <Box dir="row" padTop={20} padBottom={30} gap={20}>
-    <AdminUserSection />
   </Box>
 );
