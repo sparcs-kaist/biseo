1b54fe65cc675464d251518a846667405b0a6558
minjoo0729 <minjoo0729@kaist.ac.kr>
2023-08-22 15:12:38 +0900

    fix(tag): fix UserTag size

--- client/src/components/atoms/UserTag.tsx
+++ client/src/components/atoms/UserTag.tsx
@@ -1,11 +1,15 @@
 import styled from "@emotion/styled";
 
 export const UserTag = styled.div`
+  display: flex;
+  height: 18px;
+  padding: 3px 6px;
+  justify-content: center;
+  align-items: center;
   font-size: 9px;
   font-weight: 500;
   color: ${props => props.theme.colors.gray500};
   background-color: ${props => props.theme.colors.white};
   border: solid 1px ${props => props.theme.colors.gray200};
   border-radius: 5px;
-  padding: 3px 6px;
 `;
