f50a15c4d8bbc1c36179ca24e9b0f3efb5fdc62c
Nicole Lee <nicolelee2001@gmail.com>
2023-08-22 16:56:57 +0900

    feat(tag): add type for user tag

--- interface/admin/user/common.ts
+++ interface/admin/user/common.ts
@@ -2,15 +2,20 @@ import { z } from "zod";
 import { User } from "../../user";
 
 /**
+ * UserTag
+ * some description about user tag type goes here
+ */
+export const UserTag = z.object({
+  type: z.enum(["regular", "associate"]),
+});
+export type UserTag = z.infer<typeof UserTag>;
+
+/**
  * AdminUser
  * some description about admin user type goes here
  */
 export const AdminUser = User.extend({
   isAdmin: z.boolean(),
-  tags: z.array(
-    z.object({
-      type: z.enum(["regular", "associate"]),
-    }),
-  ),
+  tags: z.array(UserTag),
 });
 export type AdminUser = z.infer<typeof AdminUser>;
