7686b4948ee368cd90f510a88760c2c0ccadc499
minjoo0729 <minjoo0729@kaist.ac.kr>
2023-08-22 13:28:19 +0900

    fix(userpage): add AgendaTemplateCards ui

--- client/src/components/molecules/AgendaTemplateCards.tsx
+++ client/src/components/molecules/AgendaTemplateCards.tsx
@@ -1,30 +1,33 @@
-import { Box, Card, Text } from "@/components/atoms";
-import { AgendaTag, AgendaFoldedText } from "@/components/molecules";
-import type { AdminAgenda } from "biseo-interface/admin/agenda";
-import { useNavigate } from "react-router-dom";
+import React from "react";
+import { Box, Text, Card, Divider, Button } from "@/components/atoms";
+import { AgendaTag } from "@/components/molecules";
 
 const _tags = {
-  public: false,
-  identified: false,
-  votable: false,
+  public: true,
+  identified: true,
+  votable: true,
 };
+
+
 
 export const AgendaTemplateCards: React.FC = () => {
   return (
-    <Card primary={false} round={5}>
-      <Box gap={8}>
-        <Box gap={8}>
-          <AgendaTag
-            tags={{
-              public: _tags.public,
-              identified: _tags.identified,
-              votable: _tags.votable,
-            }}
-          />
-        </Box>
-        <Box w={340}>
+    <Card round={5}>
+      <Box gap={8} w="fill">
+        <AgendaTag tags={_tags} admin />
+        <Box gap={11}>
           <Text variant="title2" color="gray600">
-            😎 2023년도 정회원
+            😎 정회원 승급 심사 템플릿
+          </Text>
+        </Box>
+        <Divider />
+
+        <Box w={340} gap={2}>
+          <Text variant="title3" color="gray500">
+            [본명] 준회원의 정회원 승급심사
+          </Text>
+          <Text variant="subtitle" color="gray400">
+            [본명] 준회원의 [n]번째 정회원 승급심사입니다.
           </Text>
         </Box>
       </Box>
