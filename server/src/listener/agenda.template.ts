7578bc6499792035670b9875db509cc26c104735
Nicole Lee <nicolelee2001@gmail.com>
2023-08-24 01:37:11 +0900

    feat(template): implement agenda template apis

---
+++ server/src/listener/agenda.template.ts
@@ -0,0 +1,39 @@
+import * as schema from "biseo-interface/agenda/template";
+
+import {
+  createTemplate,
+  retrieveAll,
+  updateTemplate,
+  deleteTemplate,
+} from "@/service/agenda.template";
+
+import { Router } from "@/lib/listener";
+
+const router = Router();
+
+router.on("agenda.template.create", schema.Create, async (req, { io }) => {
+  const { templateWithChoices } = await createTemplate(req);
+
+  io.emit("agenda.template.created", templateWithChoices);
+  return {};
+});
+
+router.on("agenda.template.retrieveAll", schema.RetrieveAll, async req => {
+  return await retrieveAll();
+});
+
+router.on("agenda.template.update", schema.Update, async (req, { io }) => {
+  const { templateWithChoices } = await updateTemplate(req);
+
+  io.emit("agenda.template.updated", templateWithChoices);
+  return {};
+});
+
+router.on("agenda.template.delete", schema.Delete, async (req, { io }) => {
+  const deletedTemplate = await deleteTemplate(req);
+
+  io.emit("agenda.template.deleted", deletedTemplate);
+  return {};
+});
+
+export { router as agendaTemplateRouter };
