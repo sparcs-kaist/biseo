12caecf1c83ab66fe9fea78baf34b73175e011fc
prectal123 <prectal123@naver.com>
2023-08-24 14:10:19 +0900

    feat(ui): add template create modal navigation

--- client/src/components/organisms/AdminUserSection.tsx
+++ client/src/components/organisms/AdminUserSection.tsx
@@ -6,27 +6,46 @@ import {
   AgendaTemplateCards,
   AddButtonCard,
 } from "@/components/molecules";
+import { useAgendaTemplate } from "@/services/agenda-template";
+import { useNavigate } from "react-router-dom";
 
-export const AdminUserSection: React.FC = () => (
-  <Box dir="row" gap={20}>
-    <Box dir="column" w={380}>
-      <SectionHeader count={3}>투표 템플릿</SectionHeader>
-      <Box dir="column" w="fill" gap={15}>
+export const AdminUserSection: React.FC = () => {
+  const { templates } = useAgendaTemplate(state => ({
+    templates: state.agendaTemplates,
+  }));
+  const navigate = useNavigate();
+
+  return (
+    <Box dir="row" gap={20}>
+      <Box dir="column" w={380}>
+        <SectionHeader count={3}>투표 템플릿</SectionHeader>
         <Box dir="column" w="fill" gap={15}>
-          <AddButtonCard content="새로운 템플릿" />
+          <Box dir="column" w="fill" gap={15}>
+            <AddButtonCard
+              content="새로운 템플릿"
+              onClick={() => navigate("templateCreate")}
+            />
+          </Box>
+          {templates.map(template => (
+            <AgendaTemplateCards
+              id={template.id}
+              templateTitle={template.templateName}
+              title={template.title}
+              content={template.content}
+            />
+          ))}
         </Box>
-        <AgendaTemplateCards />
+      </Box>
+      <Box dir="column" w={380}>
+        <SectionHeader count={4}>유저 태그</SectionHeader>
+        <Box dir="column" w="fill" gap={15}>
+          <Box dir="column" w="fill" gap={15}>
+            <AddButtonCard content="새로운 태그" />
+          </Box>
+          <UserTagCards />
+          <UserTagCards />
+        </Box>
       </Box>
     </Box>
-    <Box dir="column" w={380}>
-      <SectionHeader count={4}>유저 태그</SectionHeader>
-      <Box dir="column" w="fill" gap={15}>
-        <Box dir="column" w="fill" gap={15}>
-          <AddButtonCard content="새로운 태그" />
-        </Box>
-        <UserTagCards />
-        <UserTagCards />
-      </Box>
-    </Box>
-  </Box>
-);
+  );
+};
