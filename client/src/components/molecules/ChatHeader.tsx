e3207144da714efffd259e2ce2650503a1af3554
Nicole Lee <nicolelee2001@gmail.com>
2023-08-25 23:44:09 +0900

    fix(ui): squished chat header

--- client/src/components/molecules/ChatHeader.tsx
+++ client/src/components/molecules/ChatHeader.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { Box, Text } from "@/components/atoms";
+import styled from "@emotion/styled";
 
 interface Props {
   title: string;
 }
 
 export const ChatHeader: React.FC<Props> = ({ title }) => (
-  <Box w="fill" h={42} bg="gray100" padHorizontal={20} justify="center">
+  <Container>
     <Text variant="title2">{title}</Text>
-  </Box>
+  </Container>
 );
+
+const Container = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: "100%";
+  min-height: 42px;
+  background-color: ${props => props.theme.colors.gray100};
+  padding: 0 20px;
+  justify-content: center;
+`;
