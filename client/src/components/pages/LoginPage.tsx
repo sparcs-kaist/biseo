89894f50b20d28ffd5298648fec05c92ddda7490
hye1ee <hyewon0809@kaist.ac.kr>
2023-08-26 17:26:00 +0900

    style(input): styled input container

--- client/src/components/pages/LoginPage.tsx
+++ client/src/components/pages/LoginPage.tsx
@@ -3,6 +3,8 @@ import { Navigate } from "react-router-dom";
 
 import { useAuth } from "@/services/auth";
 import { useInput } from "@/common/hooks";
+import styled from "@emotion/styled";
+import { theme } from "@/theme";
 
 export const LoginPage: React.FC = () => {
   const { login, isLoggedIn } = useAuth(state => ({
@@ -27,13 +29,65 @@ export const LoginPage: React.FC = () => {
   if (isLoggedIn) return <Navigate to="/" replace={true} />;
 
   return (
-    <div>
+    <Page>
       <h1>Login Page</h1>
       <form onSubmit={handleLogin}>
-        <input type="text" {...username} />
-        <input type="password" {...password} />
+        <InputWrapper>
+          <InputContainer
+            type="text"
+            placeholder="아이디를 입력하세요"
+            {...username}
+          />
+          <InputContainer
+            type="password"
+            placeholder="비밀번호를 입력하세요"
+            {...password}
+          />
+        </InputWrapper>
+
         <button>Login</button>
       </form>
-    </div>
+    </Page>
   );
 };
+
+const Page = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const InputWrapper = styled.div`
+  width: fit-content;
+  height: fit-content;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+`;
+
+const InputContainer = styled.input`
+  width: 310px;
+  height: 40px;
+
+  box-sizing: border-box;
+  padding: 9px 12px;
+
+  border: 1px solid ${theme.colors["gray300"]};
+  border-radius: 5px;
+  outline: none;
+
+  &:focus {
+    border: 1px solid ${theme.colors["blue300"]};
+    box-shadow: 0px 0px 0px 3px ${theme.colors["blue200"]};
+  }
+  &::placeholder {
+    font-size: 13px;
+  }
+`;
