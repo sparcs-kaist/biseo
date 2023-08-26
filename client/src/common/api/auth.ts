792d0382d25cd66edfe83eb307f851f1e8e2b82e
Soonho Kwon <percy3368@gmail.com>
2023-08-26 19:35:31 +0900

    chore(env): add api url environment variable

--- client/src/common/api/auth.ts
+++ client/src/common/api/auth.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
+import { API_URL } from "@/env";
 
 export const getToken = async (username: string, password: string) => {
   try {
-    const res = await axios.post<{ token: string }>(
-      "/api/auth/login", { username, password },
-    );
+    const res = await axios.post<{ token: string }>(`${API_URL}/auth/login`, {
+      username,
+      password,
+    });
     return res.data.token;
   } catch {
     return null;
--- client/src/socket/io.ts
+++ client/src/socket/io.ts
@@ -2,10 +2,11 @@ import { io } from "socket.io-client";
 
 import type { BiseoSocket } from "./types";
 import { emitAsync } from "./utils";
+import { API_URL } from "@/env";
 
 export const socket = Object.assign(
   io({
-    path: "/api/socket",
+    path: `${API_URL}/socket`,
     autoConnect: false,
     transports: ["websocket"],
   }) as BiseoSocket,
