7d4687a9dcc4baf63e61ff91151eea65034ba5fe
Soonho Kwon <percy3368@gmail.com>
2023-08-26 19:55:24 +0900

    fix(socket): apply ws scheme to socket endpoint

--- client/src/env.ts
+++ client/src/env.ts
@@ -1,1 +1,2 @@
 export const API_URL = import.meta.env.VITE_API_URL ?? "/api";
+export const SOCKET_URL = API_URL.replace(/^http/, "ws") + "/socket";
--- client/src/socket/io.ts
+++ client/src/socket/io.ts
@@ -2,11 +2,11 @@ import { io } from "socket.io-client";
 
 import type { BiseoSocket } from "./types";
 import { emitAsync } from "./utils";
-import { API_URL } from "@/env";
+import { SOCKET_URL } from "@/env";
 
 export const socket = Object.assign(
   io({
-    path: `${API_URL}/socket`,
+    path: SOCKET_URL,
     autoConnect: false,
     transports: ["websocket"],
   }) as BiseoSocket,
