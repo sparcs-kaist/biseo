85349a3e1982f9b2d138986a2050c205f786d511
Soonho Kwon <percy3368@gmail.com>
2023-08-26 18:41:17 +0900

    chore: remove commented out codes

--- client/src/components/templates/PageLayout.tsx
+++ client/src/components/templates/PageLayout.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { Box } from "@/components/atoms";
 import { Header } from "@/components/organisms";
 import styled from "@emotion/styled";
 
 export const PageLayout: React.FC = () => (
   <Page>
     <Header />
-    {/*<Box as="main" align="center" justify="flex-start" w="fill">*/}
     <main>
       <Outlet />
     </main>
-    {/*</Box>*/}
   </Page>
 );
 
