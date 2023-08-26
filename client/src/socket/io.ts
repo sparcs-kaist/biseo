11e56c0daf5c5d09ccc7307d6cccb3ab1cadbebb
Soonho Kwon <percy3368@gmail.com>
2023-08-26 22:26:20 +0900

    fix(preview): fix api base

--- client/src/socket/io.ts
+++ client/src/socket/io.ts
@@ -2,11 +2,12 @@ import { io } from "socket.io-client";
 
 import type { BiseoSocket } from "./types";
 import { emitAsync } from "./utils";
-import { SOCKET_URL } from "@/env";
+
+import { API_BASE } from "@/env";
 
 export const socket = Object.assign(
-  io({
-    path: SOCKET_URL,
+  io(API_BASE, {
+    path: "/api/socket",
     autoConnect: false,
     transports: ["websocket"],
   }) as BiseoSocket,
