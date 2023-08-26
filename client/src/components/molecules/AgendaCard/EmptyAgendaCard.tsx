54ff38bf352f48e807e4314ebc2b75e6d24f9c0c
Soonho Kwon <percy3368@gmail.com>
2023-08-26 15:56:49 +0900

    fix(background): apply background color

--- client/src/components/molecules/AgendaCard/EmptyAgendaCard.tsx
+++ client/src/components/molecules/AgendaCard/EmptyAgendaCard.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Text, Card } from "@/components/atoms";
 import { AgendaStatus } from "@biseo/interface/agenda";
+import styled from "@emotion/styled";
 
-const mention = {
+const mention: Record<AgendaStatus, string> = {
   ongoing: "진행중인",
   terminated: "종료된",
   preparing: "예정된",
@@ -12,19 +13,15 @@ interface Props {
   agendaStatus: AgendaStatus;
 }
 
-const findMention = (agendaStatus: AgendaStatus) => {
-  if (agendaStatus === "ongoing") return mention.ongoing;
-  if (agendaStatus === "preparing") return mention.preparing;
-  if (agendaStatus === "terminated") return mention.terminated;
-  return "";
-};
+export const EmptyAgendaCard: React.FC<Props> = ({ agendaStatus }) => (
+  <DashedCard align="center">
+    <Text variant="subtitle" color="gray500">
+      {mention[agendaStatus]} 투표가 없습니다.
+    </Text>
+  </DashedCard>
+);
 
-export const EmptyAgendaCard: React.FC<Props> = ({ agendaStatus }) => {
-  return (
-    <Card borderStyle="dashed" align="center">
-      <Text variant="subtitle" color="gray500">
-        {findMention(agendaStatus)} 투표가 없습니다.
-      </Text>
-    </Card>
-  );
-};
+const DashedCard = styled(Card)`
+  border-style: dashed;
+  background: none;
+`;
