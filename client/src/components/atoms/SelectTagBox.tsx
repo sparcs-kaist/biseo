48dcbb77c78997557530add08c00cb6601b3c428
ariaspect <keeminjoon@kaist.ac.kr>
2023-08-28 00:49:59 -0400

    feat: add react-select

--- client/src/components/atoms/SelectTagBox.tsx
+++ client/src/components/atoms/SelectTagBox.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+
+import { css } from "@emotion/react";
+import { theme } from "@/theme";
+
 import { useUserTag } from "@/services/user-tag";
 import { PositionedDownArrowIcon, SelectWrapper, TagSelect } from "./Label";
+
+import Select from "react-select";
 
 interface Props {
   selected: string[];
@@ -12,8 +18,52 @@ export const SelectTagBox: React.FC<Props> = ({ selected, onChange }) => {
     tags: state.userTags,
   }));
 
+  const tags_1 = ["test1", "test2", "test3"];
+
+  const controlStyles = css`
+    display: flex;
+    padding: 0 10px;
+    justify-content: space-between;
+    align-items: center;
+    border-radius: 5px;
+    border: 1px solid ${theme.colors.gray200};
+    background-color: ${theme.colors.white};
+
+    font-family: "Noto Sansf KR";
+    font-size: 10px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    color: ${theme.colors.gray600};
+  `;
+  const optionStyles = css`
+    border: 1px dotted ${theme.colors.black};
+    color: "black";
+    height: 18px;
+  `;
   return (
     <>
+      <Select
+        styles={{
+          control: base => ({
+            ...base,
+            ...controlStyles,
+          }),
+          option: base => ({
+            ...base,
+            ...optionStyles,
+          }),
+        }}
+        hideSelectedOptions={false}
+        closeMenuOnSelect={false}
+        // menuIsOpen
+        options={[
+          { value: "one", label: "One" },
+          { value: "two", label: "Two" },
+        ]}
+        placeholder="태그를 선택하세요"
+        isMulti
+      />
       <SelectWrapper>
         <TagSelect defaultValue={[]} onChange={onChange}>
           <option value="" disabled>
