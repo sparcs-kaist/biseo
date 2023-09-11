02c40c9b366be5958cf45a83ba206a21dc1d5fb8
withsang <jsa5115@naver.com>
2023-09-12 05:09:28 +0900

    refactor(style): allow w, h to accept type Size

--- client/src/styles/size.ts
+++ client/src/styles/size.ts
@@ -1,29 +1,33 @@
 import { css } from "@emotion/react";
 
+export type Size = number | "hug" | "fill";
+const calcSize = (size: Size) => {
+  if (size === "fill") return "100%";
+  if (size === "hug") return "fit-content";
+  return `${size}px`;
+};
+
 const applyAttribute = (attribute: "height" | "width") =>
   Object.assign(
-    (value: number) => css`
-      ${attribute}: ${value}px;
+    (value: Size) => css`
+      ${attribute}: ${calcSize(value)};
     `,
-    {
-      fill: css`
-        ${attribute}: 100%;
-      `,
-    },
   );
 
 /**
  * Applies height
  * @example
- * h(10)   // 10px height
- * h.fill  // 100% height
+ * h(10)      // 10px height
+ * h("fill")  // 100% height
+ * h("hug")   // fit-content
  */
 export const h = applyAttribute("height");
 
 /**
  * Applies width
  * @example
- * w(10)   // 10px width
- * w.fill  // 100% width
+ * w(10)      // 10px width
+ * w("fill")  // 100% width
+ * w("hug")   // fit-content
  */
 export const w = applyAttribute("width");
