45ab5698232985ac2e7444912b61b68634328d8d
withsang <jsa5115@naver.com>
2023-09-14 03:34:24 +0900

    feat(zod): add missing string length validations

--- interface/src/chat/common.ts
+++ interface/src/chat/common.ts
@@ -2,9 +2,6 @@
 import { z } from "zod";
 import { ChatUser } from "../user";
 
-/** @constant 클라이언트와 서버에서 사용하는 채팅 메시지의 최대 길이를 지정합니다. */
-const maxMessageLength = 500;
-
 /**
  * Message
  * some description about message schema goes here
@@ -13,7 +10,7 @@ export const Message = z.object({
   id: z.number(),
   user: ChatUser,
   type: z.enum(["message", "notice"]),
-  message: z.string().min(1).max(maxMessageLength),
+  message: z.string().min(1).max(500),
   createdAt: z.string().datetime(),
 });
 export type Message = z.infer<typeof Message>;
