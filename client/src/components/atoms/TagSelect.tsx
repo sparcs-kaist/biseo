4fd25097e3ff9ca08ce8ea9e768530632907235f
ariaspect <keeminjoon@kaist.ac.kr>
2023-09-14 10:02:39 -0400

    feat(css): select style tokenize

--- client/src/components/atoms/TagSelect.tsx
+++ client/src/components/atoms/TagSelect.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { css } from "@emotion/react";
-import { theme } from "@/theme";
-import { SmallCloseIcon } from "@/assets";
+import { CloseSmallIcon } from "@/assets";
 
 import Select, { components } from "react-select";
 import type {
@@ -10,6 +9,22 @@ import type {
   MultiValueGenericProps,
   MultiValueRemoveProps,
 } from "react-select";
+
+import {
+  bg,
+  border,
+  round,
+  center,
+  column,
+  row,
+  justify,
+  margin,
+  padding,
+  gap,
+  text,
+  w,
+  h,
+} from "@/styles";
 
 import { PositionedDownArrowIcon } from "./Label";
 import { PresetOption } from "./PresetOption";
@@ -27,118 +42,92 @@ interface Tag {
   description: string;
 }
 
-const controlStyles = css`
-  display: flex;
-  width: 300px;
-  height: 38px;
-  padding: 0 10px;
-  justify-content: space-between;
-  align-items: center;
-  border-radius: 5px;
-  border: 1px solid ${theme.colors.gray200};
-  background-color: ${theme.colors.gray100};
-
-  font-family: "Noto Sansf KR";
-  font-size: 11px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: normal;
-`;
-const optionStyles = css`
-  gap: 5px;
-`;
-const placeholderStyles = css`
-  color: ${theme.colors.gray300};
-`;
-const valueContainerStyles = css`
-  display: flex;
-  padding: 0px;
-  gap: 5px;
-`;
-const menuListStyles = css`
-  margin-top: 5px;
+const boxShadow = css`
   box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.06);
-  border-radius: 5px;
-  background-color: white;
-  display: flex;
-  flex-direction: column;
-  padding: 5px;
-  gap: 5px;
 `;
-const multiValueStyles = css``;
-const multiValueRemoveStyles = css`
-  display: flex;
-`;
-const Option = ({ data, isSelected, ...rest }: OptionProps<Tag>) => (
-  <components.Option {...{ data, isSelected, ...rest }}>
-    <PresetOption tag={data} selected={isSelected} />
-  </components.Option>
-);
+
+const controlStyles = css([
+  center,
+  w(300),
+  h(38),
+  padding.horizontal(10),
+  justify.between,
+  border.gray200,
+  round.md,
+  bg.gray100,
+  text.subtitle,
+]);
+const placeholderStyles = css([text.subtitle, text.gray300, padding.left(5)]);
+const valueContainerStyles = css([row, gap(5)]);
+const menuListStyles = css([
+  column,
+  gap(5),
+  margin.top(5),
+  padding(5),
+  round.md,
+  bg.white,
+  boxShadow,
+]);
+const multiValueRemoveStyles = css(center);
+
+const selectStyles = {
+  control: () => ({ ...controlStyles }),
+  placeholder: () => ({ ...placeholderStyles }),
+  valueContainer: () => ({ ...valueContainerStyles }),
+  menuList: () => ({ ...menuListStyles }),
+  multiValueRemove: () => ({ ...multiValueRemoveStyles }),
+};
+
+const Option = (props: OptionProps<Tag>) => {
+  const { data, isSelected } = props;
+  return (
+    <components.Option {...props}>
+      <PresetOption tag={data} selected={isSelected} />
+    </components.Option>
+  );
+};
 const DropdownIndicator = (props: DropdownIndicatorProps<Tag>) => (
   <components.DropdownIndicator {...props}>
     <PositionedDownArrowIcon />
   </components.DropdownIndicator>
 );
-const MultiValueContainer = ({
-  data,
-  ...rest
-}: MultiValueGenericProps<Tag>) => (
-  <components.MultiValueContainer {...{ data, ...rest }}>
-    <BorderedBox
-      dir="row"
-      round={5}
-      borderSize={1}
-      borderStyle="solid"
-      borderColor="gray200"
-      bg="white"
-      align="center"
-      gap={6}
-      padHorizontal={8}
-      padVertical={5}
-    >
-      <Text variant="option1" color="gray500">
-        {data.title}
-      </Text>
-      <components.MultiValueRemove {...{ data, ...rest }} />
-    </BorderedBox>
-  </components.MultiValueContainer>
-);
+const MultiValueContainer = (props: MultiValueGenericProps<Tag>) => {
+  const { data } = props;
+  return (
+    <components.MultiValueContainer {...props}>
+      <BorderedBox
+        dir="row"
+        round={5}
+        borderSize={1}
+        borderStyle="solid"
+        borderColor="gray200"
+        bg="white"
+        align="center"
+        gap={6}
+        padHorizontal={8}
+        padVertical={5}
+      >
+        <Text variant="option1" color="gray500">
+          {data.title}
+        </Text>
+        <components.MultiValueRemove {...props} />
+      </BorderedBox>
+    </components.MultiValueContainer>
+  );
+};
 const MultiValueRemove = (props: MultiValueRemoveProps<Tag>) => (
   <components.MultiValueRemove {...props}>
-    <SmallCloseIcon />
+    <CloseSmallIcon />
   </components.MultiValueRemove>
 );
 
 export const TagSelect: React.FC<Props> = ({ tags, onChange }) => (
   <Select
-    styles={{
-      control: () => ({
-        ...controlStyles,
-      }),
-      option: () => ({
-        ...optionStyles,
-      }),
-      placeholder: () => ({
-        ...placeholderStyles,
-      }),
-      valueContainer: () => ({
-        ...valueContainerStyles,
-      }),
-      menuList: () => ({
-        ...menuListStyles,
-      }),
-      multiValue: () => ({
-        ...multiValueStyles,
-      }),
-      multiValueRemove: () => ({
-        ...multiValueRemoveStyles,
-      }),
-    }}
+    styles={selectStyles}
     components={{
       Option,
       DropdownIndicator,
       IndicatorSeparator: () => null,
-      // MultiValue,
       MultiValueRemove,
       MultiValueLabel: () => null,
       MultiValueContainer,
@@ -147,9 +136,9 @@ export const TagSelect: React.FC<Props> = ({ tags, onChange }) => (
     closeMenuOnSelect={false}
     isSearchable={false}
     isClearable={false}
-    // menuIsOpen
     menuPortalTarget={document.body}
     unstyled
+    menuIsOpen
     options={tags}
     placeholder="태그를 선택하세요"
     onChange={selection => onChange(selection.map(t => t.title))}
