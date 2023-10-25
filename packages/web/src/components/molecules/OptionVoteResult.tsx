45d8e4f91a6bf6645c9342b81c924bb157490aa6
rjsdn0 <rjsdn@sparcs.org>
2023-10-25 20:42:53 +0900

    fix: vote color

--- packages/web/src/components/molecules/OptionVoteResult.tsx
+++ packages/web/src/components/molecules/OptionVoteResult.tsx
@@ -37,7 +37,7 @@ export const OptionVoteResult: React.FC<Props> = ({
   <Box w="fill" h={30}>
     <Background
       percent={(count / totalCount) * 100}
-      color={userChoice ? "blue200" : "blue100"}
+      color={userChoice ? "blue500" : "blue300"}
       borderColor={userChoice ? "blue300" : "gray200"}
     >
       <Box
