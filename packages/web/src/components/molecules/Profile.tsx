8013ec30795da69213eb4c1bba7fb485ebba18c2
minjoo0729 <minjoo0729@kaist.ac.kr>
2023-10-25 14:43:37 +0900

    fix(profile): connect position of profile relative to box

--- packages/web/src/components/molecules/Profile.tsx
+++ packages/web/src/components/molecules/Profile.tsx
@@ -21,42 +21,40 @@ type Props = {
 
 export const Profile: React.FC<Props> = ({ displayName, onLogout }) => (
   <div
-    css={[
-      border.gray200,
-      round.md,
-      w(100),
-      column,
-      align.center,
-      gap(4),
-      padding.vertical(5),
-      bg.white,
-      "box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.06)",
-      {
-        position: "absolute",
-        top: "80%",
-        left: "96.2%",
-        transform: "translateX(-100%)",
-        zIndex: 1000,
-      },
-    ]}
+    css={[align.center, { position: "absolute", right: "0px", zIndex: 1000 }]}
   >
+    <div css={[h(5)]} />
     <div
       css={[
+        border.gray200,
         round.md,
         w(100),
-        h(18),
-        padding.horizontal(8),
-        justify.between,
+        column,
         align.center,
+        gap(4),
+        padding.vertical(5),
+        bg.white,
+        "box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.06)",
       ]}
     >
-      <div css={[text.option2, text.gray600, "text-align: center"]}>
-        {displayName}
+      <div
+        css={[
+          round.md,
+          w(100),
+          h(18),
+          padding.horizontal(8),
+          justify.between,
+          align.center,
+        ]}
+      >
+        <div css={[text.option2, text.gray600, "text-align: center"]}>
+          {displayName}
+        </div>
       </div>
+      <Divider />
+      <Button w={90} h={20} padHorizontal={8} onClick={onLogout}>
+        <div css={[text.option2, text.blue600]}>로그아웃</div>
+      </Button>
     </div>
-    <Divider />
-    <Button w={90} h={20} padHorizontal={8} onClick={onLogout}>
-      <div css={[text.option2, text.blue600]}>로그아웃</div>
-    </Button>
   </div>
 );
