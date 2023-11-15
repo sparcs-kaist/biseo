f6d2b521c6ba26322e97fa7b3a01fd5af0ff1659
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-11-15 21:51:11 +0900

    fix: remove themeProvider

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,11 +1,9 @@
+import { enableMapSet } from "immer";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { ThemeProvider } from "@emotion/react";
-import { enableMapSet } from "immer";
 
 import router from "@biseo/web/router";
-import { theme } from "@biseo/web/theme";
 
 import "./index.css";
 
@@ -13,8 +11,6 @@ enableMapSet();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
