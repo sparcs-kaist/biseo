d93af0a4c4ad868a30854c9304d1bf730fa985f0
rjsdn0 <rjsdn@sparcs.org>
2024-02-15 00:23:16 +0900

    refactor: updated votedetail

--- packages/web/src/components/molecules/VoteDetail.tsx
+++ packages/web/src/components/molecules/VoteDetail.tsx
@@ -2,20 +2,14 @@ import React from "react";
 import { Box, Text, Tag } from "@biseo/web/components/atoms";
 
 interface Props {
-  type: boolean;
+  named: boolean;
 }
 
-export const VoteDetail: React.FC<Props> = ({ type }) => (
-  <>
-    {type ? (
-      <>TODO</>
-    ) : (
-      <Box w="fill" justify="space-between" dir="row" align="center">
-        <Text variant="subtitle" color="black">
-          투표 상세
-        </Text>
-        <Tag type="anonymous" />
-      </Box>
-    )}
-  </>
+export const VoteDetail: React.FC<Props> = ({ named }) => (
+  <Box w="fill" justify="space-between" dir="row" align="center">
+    <Text variant="subtitle" color="black">
+      투표 상세
+    </Text>
+    <Tag type={named ? "identified" : "anonymous"} />
+  </Box>
 );
