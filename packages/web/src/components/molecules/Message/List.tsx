aa5148df4d5742bc4e011b2595e3ef655a9cd8a8
Chaemin Lee <140066207+lunariana@users.noreply.github.com>
2024-05-05 22:25:32 +0900

    feat: refactor message/list

--- packages/web/src/components/molecules/Message/List.tsx
+++ packages/web/src/components/molecules/Message/List.tsx
@@ -1,9 +1,11 @@
 import styled from "@emotion/styled";
-import { Scroll } from "@biseo/web/components/atoms";
+import { scroll, scrollBar } from "@biseo/web/styles";
 
-export const List = styled(Scroll)`
+export const List = styled.div`
   display: flex;
   flex-direction: column-reverse;
   flex-grow: 1;
   background: ${({ theme }) => theme.colors.white};
+  ${scroll.y}
+  ${scrollBar}
 `;
