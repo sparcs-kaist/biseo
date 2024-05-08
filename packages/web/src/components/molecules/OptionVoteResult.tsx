bbae9702c522a2b66cca9c50b9c748297bbf1d90
Chaemin Lee <140066207+lunariana@users.noreply.github.com>
2024-05-09 00:52:38 +0900

    feat: refactor option vote result

--- packages/web/src/components/molecules/OptionVoteResult.tsx
+++ packages/web/src/components/molecules/OptionVoteResult.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { Box, Text } from "@biseo/web/components/atoms";
 import type { Color } from "@biseo/web/theme";
+import { w, h, row, align, justify, padding, text } from "@biseo/web/styles";
 
 interface Props {
   name: string;
@@ -34,28 +34,30 @@ export const OptionVoteResult: React.FC<Props> = ({
   totalCount,
   userChoice = false,
 }) => (
-  <Box w="fill" h={30}>
+  <div css={[w("fill"), h(30)]}>
     <Background
       percent={(count / totalCount) * 100}
       color={userChoice ? "blue300" : "blue200"}
       borderColor={userChoice ? "blue300" : "gray200"}
     >
-      <Box
-        w="fill"
-        h="fill"
-        dir="row"
-        align="center"
-        justify="space-between"
-        padHorizontal={13}
-        padVertical={6}
+      <div
+        css={[
+          w("fill"),
+          h("fill"),
+          row,
+          align.center,
+          justify.between,
+          padding.horizontal(13),
+          padding.vertical(6),
+        ]}
       >
-        <Text color={userChoice ? "blue500" : "gray500"} variant="option1">
+        <p css={[text.option1, text[userChoice ? "blue500" : "gray500"]]}>
           {name}
-        </Text>
-        <Text color={userChoice ? "blue500" : "gray500"} variant="option1">
+        </p>
+        <p css={[text.option1, text[userChoice ? "blue500" : "gray500"]]}>
           {count}
-        </Text>
-      </Box>
+        </p>
+      </div>
     </Background>
-  </Box>
+  </div>
 );
