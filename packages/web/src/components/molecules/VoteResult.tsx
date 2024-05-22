6bec63b1b2a43ea0b1d5033cb3a0d35561c2bc85
Chaemin Lee <140066207+lunariana@users.noreply.github.com>
2024-05-22 20:39:48 +0900

    feat: refactor vote result

--- packages/web/src/components/molecules/VoteResult.tsx
+++ packages/web/src/components/molecules/VoteResult.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { Box, Text, Tag } from "@biseo/web/components/atoms";
+import { Tag } from "@biseo/web/components/atoms";
+import { align, row, gap, text, justify, w } from "@biseo/web/styles";
+import { css } from "@emotion/react";
 
 interface Props {
   type?: boolean;
@@ -13,25 +15,31 @@ export const VoteResult: React.FC<Props> = ({
   clickHandler,
   revealChoice,
   voted,
-}) => (
-  <Box w="fill" justify="space-between" dir="row" align="center">
-    <Box gap={5} dir="row" align="center">
-      <Text variant="subtitle" color="black">
-        투표 결과
-      </Text>
-      {voted ? (
-        <Text
-          variant="option2"
-          color="gray400"
-          onClick={e => {
-            clickHandler(revealChoice);
-            e.stopPropagation();
-          }}
-        >
-          {!revealChoice ? "내 투표 보기" : "내 투표 가리기"}
-        </Text>
-      ) : null}
-    </Box>
-    <Tag type={type ? "public" : "private"} />
-  </Box>
-);
+}) => {
+  const textButton = css`
+    border: none;
+    background-color: inherit;
+    cursor: pointer;
+  `;
+
+  return (
+    <div css={[w("fill"), justify.between, row, align.center]}>
+      <div css={[gap(5), row, align.center]}>
+        <h4 css={[text.subtitle, text.black]}>투표 결과</h4>
+        {voted ? (
+          <button
+            type="button"
+            css={[text.option2, text.gray400, textButton]}
+            onClick={e => {
+              clickHandler(revealChoice);
+              e.stopPropagation();
+            }}
+          >
+            {!revealChoice ? "내 투표 보기" : "내 투표 가리기"}
+          </button>
+        ) : null}
+      </div>
+      <Tag type={type ? "public" : "private"} />
+    </div>
+  );
+};
