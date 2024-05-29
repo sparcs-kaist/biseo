a8b825fd6b6167b32331d9ed4afaed75e453ee28
Chaemin Lee <140066207+lunariana@users.noreply.github.com>
2024-05-29 15:38:09 +0900

    feat: refactor admin setting section

--- packages/web/src/components/organisms/AdminSettingSection.tsx
+++ packages/web/src/components/organisms/AdminSettingSection.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Box, Scroll } from "@biseo/web/components/atoms";
 import {
   SectionHeader,
   UserTagCards,
@@ -9,6 +8,7 @@ import {
 } from "@biseo/web/components/molecules";
 import { useAgendaTemplate } from "@biseo/web/services/agenda-template";
 import { useUserTag } from "@biseo/web/services/user-tag";
+import { column, gap, h, row, w, scroll, scrollBar } from "@biseo/web/styles";
 
 export const AdminSettingSection: React.FC = () => {
   const { templates } = useAgendaTemplate(state => ({
@@ -21,44 +21,44 @@ export const AdminSettingSection: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <Box dir="row" h="fill" gap={20}>
-      <Box dir="column" w={380} h="fill">
+    <div css={[row, h("fill"), gap(20)]}>
+      <div css={[column, w(380), h("fill")]}>
         <SectionHeader count={templates.length}>투표 템플릿</SectionHeader>
-        <Box dir="column" w="fill" h="fill" gap={15}>
+        <div css={[column, w("fill"), h("fill"), gap(15)]}>
           <AddButtonCard
             content="새로운 템플릿"
             onClick={() => navigate("templateCreate")}
           />
-          <Box dir="column" h={600}>
-            <Scroll>
-              <Box dir="column" gap={15}>
+          <div css={[column, h(600)]}>
+            <div css={[scroll.y, scrollBar]}>
+              <div css={[column, gap(15)]}>
                 {templates.map(template => (
                   <AgendaTemplateCard key={template.id} template={template} />
                 ))}
-              </Box>
-            </Scroll>
-          </Box>
-        </Box>
-      </Box>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-      <Box dir="column" w={380} h="fill">
+      <div css={[column, w(380), h("fill")]}>
         <SectionHeader count={tags.length}>유저 태그</SectionHeader>
-        <Box dir="column" w="fill" h="fill" gap={15}>
+        <div css={[column, w("fill"), h("fill"), gap(15)]}>
           <AddButtonCard
             content="새로운 태그"
             onClick={() => navigate("tagCreate")}
           />
-          <Box dir="column" h={600}>
-            <Scroll>
-              <Box dir="column" gap={15}>
+          <div css={[column, h(600)]}>
+            <div css={[scroll.y, scrollBar]}>
+              <div css={[column, gap(15)]}>
                 {tags.map(usertag => (
                   <UserTagCards key={usertag.id} tag={usertag} />
                 ))}
-              </Box>
-            </Scroll>
-          </Box>
-        </Box>
-      </Box>
-    </Box>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
