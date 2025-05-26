6adb67d27fab077ea8f8292e47bfa30115c3ba36
rjsdn0 <rjsdn@sparcs.org>
2025-05-26 19:41:10 +0900

    fix: remove ldap login

--- packages/api/src/auth/router.ts
+++ packages/api/src/auth/router.ts
@@ -2,10 +2,13 @@ import { Router } from "express";
 import { loginHandler, gLoginHandler } from "./login";
 
 const router = Router();
-
+/**
+ * @deprecated Use google authentication instead
+ */
 router.post("/login", (req, res, next) => {
   loginHandler(req, res).catch(next);
 });
+
 router.post("/glogin", (req, res, next) => {
   gLoginHandler(req, res).catch(next);
 });
